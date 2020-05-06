d1a7e594448cf062e7d13d0bb913a89f9a5e9e3f
zyc <974246866@qq.com>
2020-05-06 15:47:20 +0800

    detail list

--- src/components/detail/Detail.vue
+++ src/components/detail/Detail.vue
@@ -2,23 +2,58 @@
   <div>
     <DetailBanner></DetailBanner>
     <DetailHeader></DetailHeader>
-    <div class="content"></div>
+    <div class="content">
+      <DetailList :list="list"></DetailList>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人三关联票',
+              children: [
+                {
+                  title: '成人三关联票摸一个练习'
+                }
+              ]
+            },
+            {
+              title: '成人五关联票'
+            }
+          ]
+        },
+        {
+          title: '学生票'
+        },
+        {
+          title: '儿童票'
+        },
+        {
+          title: '特惠票'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-  .content
-    height 50rem
+.content
+  height 50rem
 </style>
